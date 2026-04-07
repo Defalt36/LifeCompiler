a0d3a870cff88785fec9b03f7072d5f614ddefe2
Defalt36 <FaelOtk@gmail.com>
2026-04-07 17:06:55 -0300

    Create generalFixes.sh

---
+++ scripts/generalFixes.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Shamelessly ripped from risvh's repo
+
+cd $ASSISTANTDIR/utilities
+cp -f Makefile.MinGWCross configure $MINORDIR/game/platforms/SDL/
+
+# Fix winsock letter case (For cross-compiling client for Windows on Linux)
+sed -i 's/<Winsock.h>/<winsock.h>/' $MINORDIR/network/win32/SocketClientWin32.cpp;
+sed -i 's/<Winsock.h>/<winsock.h>/' $MINORDIR/network/win32/SocketServerWin32.cpp;
+
+# Obsolete values.h is replaced by float.h (For cross-compiling server for Windows on Linux)
+sed -i 's/<values.h>/<float.h>/' $GAMEDIR/server/map.cpp;
+# Fix winsock letter case (For cross-compiling server for Windows on Linux)
+sed -i 's/<Winsock.h>/<winsock.h>/' $MINORDIR/network/unix/SocketPollUnix.cpp;
+
+# Make sure it has Unix EOL
+find $GAMEDIR -type f \( -name 'configure' \) -exec sed -i 's/\r//g' {} +
+find $GAMEDIR -type f \( -name '*.sh' \) -exec sed -i 's/\r//g' {} +
+find $GAMEDIR -type f \( -name 'Makefile*' \) -exec sed -i 's/\r//g' {} +
+
+# libpng depends on zlib, the order of linking flag matters
+sed -i 's/PLATFORM_LIBPNG_FLAG = -lz -lpng/PLATFORM_LIBPNG_FLAG = -lpng -lz/' $MINORDIR/game/platforms/SDL/Makefile.MinGWCross
+
+# Fix to make SDL statically linked
+sed -i '/ -static `sdl-config --static-libs`$/! s/^PLATFORM_LIBPNG_FLAG .*$/& -static `sdl-config --static-libs`/' $MINORDIR/game/platforms/SDL/Makefile.MinGWCross
+
+# New versions of imagemagick flips images after conversion
+im_version=`convert -list configure | sed '/^LIB_VERSION_NUMBER */!d; s//,/;  s/,/,0/g;  s/,0*\([0-9][0-9]\)/\1/g' | head -n 1`
+if [ "$im_version" -ge "06091075" ]; then
+	sed -i 's/convert $< -type truecolormatte $@/convert -auto-orient $< -type truecolormatte $@/' $MINORDIR/game/platforms/SDL/Makefile.all
+fi
+
+# fix include windows.h letter case in discord_sdk (For cross-compiling client for Windows on Linux)
+# sed -i 's/<Windows.h>/<windows.h>/' ../dependencies/discord_game_sdk/c/discord_game_sdk.h;
