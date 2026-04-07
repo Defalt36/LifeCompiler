f8f6fe5edf0832858d71a2f551f887976b86be66
Defalt36 <FaelOtk@gmail.com>
2026-04-07 16:06:54 -0300

    Update buildLatestTHOL.sh

--- scripts/buildLatestTHOL.sh
+++ scripts/buildLatestTHOL.sh
@@ -43,26 +43,19 @@ git fetch --tags
 latestTaggedVersionB=`git for-each-ref --sort=-creatordate --format '%(refname:short)' --count=1 refs/tags/2HOL_v* | sed -e 's/2HOL_v//'`
 git checkout -q 2HOL_v$latestTaggedVersionB
 
-rm */cache.fcz
-
 cd ..
 
 latestVersion=$latestTaggedVersionB
-
-
 if [ $latestTaggedVersionA -gt $latestTaggedVersionB ]
 then
 	latestVersion=$latestTaggedVersionA
 fi
 
-echo
-echo "Building 2HOLv$latestVersion..."
-echo
+echo "\nBuilding 2HOL v$latestVersion...\n"
 
 cd $GAMEDIR
 
 chmod u+x ./configure || exit 1
-
 if [ $target == "linux" ] ; then
 	./configure 1 || exit 1
 elif [ $target == "windows" ] ; then
@@ -70,23 +63,22 @@ elif [ $target == "windows" ] ; then
 fi
 
 cd gameSource
-
 make || exit 1
-
 cd $SCRIPTSDIR
 
 RELEASEDIR="${BUILDSDIR}/2HOL_v${latestVersion}-${target}"
 mkdir $RELEASEDIR
 
+echo "\nCopying files to ${RELEASEDIR}...\n"
+
 $ASSISTANTDIR/scripts/gatherData.sh game "$RELEASEDIR" copy
 $ASSISTANTDIR/scripts/gatherBuildFiles.sh game "$RELEASEDIR"
 $ASSISTANTDIR/scripts/gatherBinaries.sh "$target" game "$RELEASEDIR"
 
-#ln -vsf $RELEASEDIR $BUILDSDIR/$RELEASENAME
+echo "\nCompressing files...\n"
 
 7z a $RELEASEDIR.zip $RELEASEDIR
 
-echo
-echo "Done building v$latestVersion."
+echo "\nDone building 2HOL v$latestVersion."
 
 
